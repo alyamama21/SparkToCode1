11aefe06b6e9adca5f5a9f26fab7d9abfcc8dbec
Alyamama <alyamamaahmed2@gmail.com>
2026-07-01 10:55:06 +0400

    Add first task

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Personal Info Card
+
+            Console.WriteLine(" Enter Your Name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Age: ");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Hight: ");
+            double userHeight = double.Parse(Console.ReadLine());
+
+            Console.Write("Are you a student? (true/false): ");
+            bool isStudent = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("---Personal Info Card---");
+            Console.WriteLine($" Name: {userName} " + 
+                $" Age: {userAge}" + 
+                $" Hight: {userHeight} " + 
+                $" Status: Student "); 
+            
+        }
+    }
+}
