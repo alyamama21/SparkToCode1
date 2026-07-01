0c21162e14b2a7c83390c1f53f00a1a8ebfcbf34
Alyamama <alyamamaahmed2@gmail.com>
2026-07-01 11:03:33 +0400

    Add first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,25 +6,36 @@
         {
             // Task 1 - Personal Info Card
 
-            Console.WriteLine(" Enter Your Name: ");
-            string userName = Console.ReadLine();
+            //Console.WriteLine(" Enter Your Name: ");
+            //string userName = Console.ReadLine();
 
-            Console.WriteLine("Enter Your Age: ");
-            int userAge = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your Age: ");
+            //int userAge = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Your Hight: ");
-            double userHeight = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Your Hight: ");
+            //double userHeight = double.Parse(Console.ReadLine());
 
-            Console.Write("Are you a student? (true/false): ");
-            bool isStudent = bool.Parse(Console.ReadLine());
+            //Console.Write("Are you a student? (true/false): ");
+            //bool isStudent = bool.Parse(Console.ReadLine());
 
-            Console.WriteLine();
-            Console.WriteLine("---Personal Info Card---");
-            Console.WriteLine($" Name: {userName} " + 
-                $" Age: {userAge}" + 
-                $" Hight: {userHeight} " + 
-                $" Status: Student "); 
-            
+            //Console.WriteLine();
+            //Console.WriteLine("---Personal Info Card---");
+            //Console.WriteLine($" Name: {userName} " + 
+            //    $" Age: {userAge}" + 
+            //    $" Hight: {userHeight} " + 
+            //    $" Status: Student "); 
+            ///////////////////////////////
+
+
+            //Task 2- Rectangle Calculator
+            Console.WriteLine("Enter the length of the rectangle: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area of the rectangle: " + (length * width) , "Perimeter = 2 * (length * width)");
+
         }
     }
 }
