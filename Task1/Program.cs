df8a6dde6b5a08f93a6242cb4fa6c55293baecc9
Alyamama <alyamamaahmed2@gmail.com>
2026-07-05 10:51:16 +0400

    implement problem 10 (Mini Calculator)

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -182,46 +182,77 @@ namespace Task1
             ////////////////////////////
             //Task 9-   Day Name Finder
             //some code to solvr problem
-            Console.WriteLine("Enter a number (1-7) to find the corresponding day of the week: ");
-            int dayNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a number (1-7) to find the corresponding day of the week: ");
+            //int dayNumber = int.Parse(Console.ReadLine());
 
-            int day = 1;
-            switch (dayNumber)
-            {
-                case 1:
-                    Console.WriteLine("The day is Sunday.");
+            //int day = 1;
+            //switch (dayNumber)
+            //{
+            //    case 1:
+            //        Console.WriteLine("The day is Sunday.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("The day is Monday.");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("The day is Tuesday.");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("The day is Wednesday.");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("The day is Thuersday.");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("The day is Friday.");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("The day is Saturday.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
+            //        break;
+
+
+            ////////////////////////////
+            //Task 10-    Mini Calculator
+            //some code to solvr problem
+            Console.WriteLine("Enter First Number : ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator (+, -, *, /): ");
+            char op = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number :");
+            int num2 = int.Parse(Console.ReadLine());
+
+            
+
+            
+            switch (op)
+                {
+            case '+':
+                    Console.WriteLine("Addition: " + (num1 + num2));
                     break;
-                case 2:
-                    Console.WriteLine("The day is Monday.");
+                case '-':
+                    Console.WriteLine("Subtraction: " + (num1 - num2));
                     break;
-                case 3:
-                    Console.WriteLine("The day is Tuesday.");
+                case '*':
+                    Console.WriteLine("Multiplication: " + (num1 * num2));
                     break;
-                case 4:
-                    Console.WriteLine("The day is Wednesday.");
-                    break;
-                case 5:
-                    Console.WriteLine("The day is Thuersday.");
-                    break;
-                case 6:
-                    Console.WriteLine("The day is Friday.");
-                    break;
-                case 7:
-                    Console.WriteLine("The day is Saturday.");
+                case '/':
+                    Console.WriteLine("Divition: " + (num1 / num2));
                     break;
                 default:
-                    Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
+                    Console.WriteLine("Unknown operator. Please enter a valid operator (+, -, *, /).");
                     break;
 
 
 
-
-
-
-
-
+                }
             }
 
         }
     }
-}
+
+
